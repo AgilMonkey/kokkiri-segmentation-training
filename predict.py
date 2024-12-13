b60a7d202848484c17f9ef8716a7fa8e84e9afd9
Aepu <muhammadagilfadilah@gmail.com>
2024-12-13 11:15:17 +0900

    Add test images

--- predict.py
+++ predict.py
@@ -1,13 +1,14 @@
 from ultralytics import YOLO
 from PIL import Image
+import glob
 import cv2
 
 
 model_path = "/home/agil/Documents/Kuliah/IISMA/Ulsan College/UC Kuliah/Practical Cases In The IT Field/Projek besar/Training Segmentation/runs/segment/train5/weights/best.pt"
-image_path = "image.jpg"
+image_path = glob.glob("image.*")
 
 
-img = cv2.imread(image_path)
+img = cv2.imread(image_path[0])
 H, W, _ = img.shape
 
 model = YOLO(model_path)
