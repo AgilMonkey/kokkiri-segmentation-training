ae80b83555088a8996354ebce7365c0a822e0915
Aepu <muhammadagilfadilah@gmail.com>
2024-12-18 16:01:57 +0900

    mod train batch

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11s-seg.pt")
-model.train(data="config.yaml", epochs=50, batch=4, imgsz=640)
+model.train(data="config.yaml", epochs=50, batch=16, imgsz=640)
